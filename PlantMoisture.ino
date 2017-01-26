6951f65915e3b7dddaa99c08d93400b781302a52
Madhav Kanbur <abcdjdj@gmail.com>
2017-01-26 19:34:53 +0530

    Add GPL V3 license headers
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- PlantMoisture.ino
+++ PlantMoisture.ino
@@ -1,3 +1,19 @@
+/* Arduino Moisture Sensing Code
+ * Copyright (C) 2016 Madhav Kanbur
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/> */
+
 #define MOISTURE_PIN 3
 #define LED_PIN 13
 #define RX_PIN 10
