d0536680fe9ecca2f9498f1d21c82a8f72eebb44
Madhav Kanbur <abcdjdj@gmail.com>
2017-01-26 17:00:33 +0530

    PlantMoisture.ino : Coding style fixes
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- PlantMoisture.ino
+++ PlantMoisture.ino
@@ -5,29 +5,29 @@
 
 #include "esp8266.h"
 
-int readMoisture(){
-    int ret = analogRead(MOISTURE_PIN);
-    return ret;
+int readMoisture()
+{
+        int ret = analogRead(MOISTURE_PIN);
+        return ret;
 }
 
-void setup() {
-    Serial.begin(9600); // Setup serial through USB
-    
-    esp_init(9600);
-    
-    pinMode(LED_PIN,OUTPUT); // Initialize on board LED
-    digitalWrite(LED_PIN,LOW); // Disable on board LED
+void setup()
+{
+        Serial.begin(9600); // Setup serial through USB
+
+        esp_init(9600);
+
+        pinMode(LED_PIN,OUTPUT); // Initialize on board LED
+        digitalWrite(LED_PIN,LOW); // Disable on board LED
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  // listen for communication from the ESP8266 and then write it to the serial monitor
-   if(esp8266.available() ){
-        Serial.write((char)esp8266.read());
-   }
- 
-    // listen for user input and send it to the ESP8266
-   if(Serial.available()){
-          esp8266.write(Serial.read());
-   }
+void loop()
+{
+        if(esp8266.available()) {
+                Serial.write((char)esp8266.read());
+        }
+
+        if(Serial.available()) {
+                esp8266.write(Serial.read());
+        }
 }
